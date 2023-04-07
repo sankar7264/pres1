67d21422e38eb1382dd0a360fccdddc36b842e91
Sankara rao Killi <sankara.killi@materialplus.io>
2023-04-07 10:40:53 +0530

    added

---
+++ .stories/config.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from '@storybook/react/demo';
+
+storiesOf('Button', module)
+  .add('with text', () => <Button>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+import { configure } from '@storybook/react';
+
+function loadStories() {
+  require('../stories/index.js');
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
